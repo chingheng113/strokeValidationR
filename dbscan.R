9716542fe565dd2d745ce036b4dc88d739011f81
aalinc9 <chingheng113@gmail.com>
2018-11-27 09:32:14 -0500

    back up

--- dbscan.R
+++ dbscan.R
@@ -4,19 +4,21 @@ library("factoextra")
 library("fpc")
 source('my_util.R')
 tsr_data <- load_tsr_data('all', '') # all, 0..5;is/he
-
+data <- tsr_data$b_data
+# data <- file.path('data', paste('pca.csv', sep='')) %>%
+  # read.csv(., header = TRUE, sep = ',')
 # distance
-dbscan::kNNdistplot(tsr_data$b_data, k = 5)
+dbscan::kNNdistplot(data, k = 5)
 abline(h = 0.15, lty = 2)
 
 
 # k-mean
-km.res = kmeans(tsr_data$b_data, 2, nstart = 25)
-fviz_cluster(km.res, tsr_data$b_data, frame = FALSE, geom = "point")
+km.res = kmeans(data, 5, nstart = 25)
+fviz_cluster(km.res, data, frame = FALSE, geom = "point")
 
 
 # dbscan
-db = fpc::dbscan(tsr_data$b_data, eps = 0.15, MinPts = 5)
-plot(db, tsr_data$b_data, main = "DBSCAN", frame = FALSE)
-fviz_cluster(db, tsr_data$b_data, stand = FALSE, frame = FALSE, geom = "point")
+db = fpc::dbscan(data, eps = 0.3, MinPts = 3)
+plot(db, data, main = "DBSCAN", frame = FALSE)
+fviz_cluster(db, data, stand = FALSE, frame = FALSE, geom = "point")
 print(db)
