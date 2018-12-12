da0f1e66e04a9ecd222c4197ec155f2317c8d195
aalinc9 <chingheng113@gmail.com>
2018-12-12 10:09:54 -0500

    back up

--- k-mean.R
+++ k-mean.R
@@ -4,18 +4,18 @@ library("factoextra")
 library("fpc")
 source('my_util.R')
 
-tsr_data <- load_tsr_data('5', 'is') # all, 0..5;is/he
+tsr_data <- load_tsr_data('1', 'is') # all, 0..5;is/he
 bData <- tsr_data$b_data
-bData <- unique(bData)
-# PCA reduction
 pca_result_tsr <- princomp(bData, cor = TRUE)
-bData <- pca_result_tsr$scores
+bData_pca <- pca_result_tsr$scores
+bData_pca <- bData_pca[,1:2]
+bData_pca_unique <- unique(bData_pca)
 
 # distance
-dbscan::kNNdistplot(bData, k = 2)
+dbscan::kNNdistplot(bData_pca_unique, k = 10)
 abline(h = 0.15, lty = 2)
 
 # k-mean
-km.res = kmeans(bData, 2, nstart = 25)
-fviz_cluster(km.res, bData, frame = FALSE, geom = "point")
+km.res = kmeans(bData_pca_unique, 10, nstart = 25)
+fviz_cluster(km.res, bData_pca_unique, frame = FALSE, geom = "point")
 
