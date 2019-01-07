323c80c1142baa6d0b7315c0822ad155bbb86642
aalinc9 <chingheng113@gmail.com>
2019-01-07 08:52:59 -0500

    back up

--- loess.R
+++ loess.R
@@ -9,4 +9,7 @@ data <- load_csv_data('TSR_2017')
 data_bm <- dplyr::select(data, Barthel_Total, discharged_mrs)
 plot(Barthel_Total ~ discharged_mrs, data=data_bm )
 
-# lowess(data_bm$Barthel_Total ~ data_bm$discharged_mrs)
+lowess.data <- lowess(data_bm$Barthel_Total ~ data_bm$discharged_mrs, f=1/32)
+
+plot(as.numeric(data_bm$discharged_mrs, data_bm$Barthel_Total),xlab="mrs",ylab="bar",main="LOWESS (f=1/32)",col=2)
+lines(lowess.data$x,lowess.data$y,lwd=2)
